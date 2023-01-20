c12ef1a512892c4cc61d6998a849bb51cf26258a
Filipe <filipegallodev@hotmail.com>
2023-01-20 14:28:14 -0300

    feat: only 6 countries appear, with an option to increase

--- src/components/Countries/Countries.tsx
+++ src/components/Countries/Countries.tsx
@@ -4,17 +4,38 @@ import { getCountries } from "@/scripts/fetchData";
 
 const Countries = () => {
   const [countries, setCountries] = React.useState<Country[]>();
+  const [maxShowCountries, setMaxShowCountries] = React.useState(6);
+  const [limitReached, setLimitReached] = React.useState(false);
 
   React.useEffect(() => {
     async function fetchData() {
       const data = await getCountries();
-      setCountries(data);
+      handleCountriesToShow(data);
     }
     fetchData();
-  }, []);
+  }, [maxShowCountries]);
+
+  function handleCountriesToShow(data: Country[]): void {
+    if (limitReached) return;
+
+    if (data && data instanceof Array && "name" in data[0]) {
+      let counter = 0;
+      const countriesToShow = data.filter((country) => {
+        if (counter < maxShowCountries) {
+          counter++;
+          return country;
+        }
+      });
+      if (maxShowCountries > countriesToShow.length) setLimitReached(true);
+      setCountries(countriesToShow);
+    }
+  }
 
   return (
     <>
+      <button onClick={() => setMaxShowCountries(maxShowCountries + 6)}>
+        More 10 countries
+      </button>
       {countries && (
         <ul>
           {countries.map((country) => (
