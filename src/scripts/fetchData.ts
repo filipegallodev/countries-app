c6fce61ced67e04ee237d3d1bb20b1fbfbea25d6
Filipe <filipegallodev@hotmail.com>
2023-01-20 14:10:14 -0300

    feat: fetch error catching

--- src/scripts/fetchData.ts
+++ src/scripts/fetchData.ts
@@ -1,5 +1,10 @@
 export async function getCountries() {
-  const response = await fetch("https://restcountries.com/v3.1/all");
-  const data = await response.json();
-  return data;
+  try {
+    const response = await fetch("https://restcountries.com/v3.1/all");
+    if (!response.ok) throw new Error("Error: " + response.status);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    if (error instanceof ErrorEvent) console.error("Error " + error.message);
+  }
 }
