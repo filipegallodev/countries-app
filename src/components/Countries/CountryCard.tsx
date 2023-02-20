c11669b5a9554e7e69edee3705a13d1c02553997
Filipe <filipegallodev@hotmail.com>
2023-02-20 09:50:07 -0300

    basic country card

--- src/components/Countries/CountryCard.tsx
+++ src/components/Countries/CountryCard.tsx
@@ -1,13 +1,29 @@
 import React from "react";
+import styled from "styled-components";
 
 const CountryCard = ({ country }: { country: Country }) => {
   return (
     <li>
-      <div>
+      <Card>
+        <Image src={country.flags.svg} alt={country.name.official} />
         <h3>{country.name.common}</h3>
-      </div>
+      </Card>
     </li>
   );
 };
+
+const Card = styled.div`
+  width: 320px;
+  text-align: center;
+  border: 1px solid #777;
+  border-radius: 16px;
+  padding: 16px;
+`;
+
+const Image = styled.img`
+  height: 100px;
+  width: auto;
+  margin-bottom: 16px;
+`;
 
 export default CountryCard;
