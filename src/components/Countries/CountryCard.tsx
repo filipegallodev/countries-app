c1e081135a281d9059e01e4b92a40c575ba0ddc4
Filipe <filipegallodev@hotmail.com>
2023-02-21 11:22:59 -0300

    card new styles

--- src/components/Countries/CountryCard.tsx
+++ src/components/Countries/CountryCard.tsx
@@ -6,24 +6,53 @@ const CountryCard = ({ country }: { country: Country }) => {
     <li>
       <Card>
         <Image src={country.flags.svg} alt={country.name.official} />
-        <h3>{country.name.common}</h3>
+        <div>
+          <Name>{country.name.common}</Name>
+          <Region>{country.region}</Region>
+        </div>
+        <div>
+          <Population>
+            Population: {country.population.toLocaleString()}
+          </Population>
+        </div>
       </Card>
     </li>
   );
 };
 
 const Card = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
   width: 320px;
+  height: 320px;
   text-align: center;
   border: 1px solid #777;
   border-radius: 16px;
   padding: 16px;
+  background-color: #eee;
 `;
 
 const Image = styled.img`
   height: 100px;
   width: auto;
   margin-bottom: 16px;
+`;
+
+const Name = styled.h3`
+  color: #222;
+  font-size: 1.5rem;
+`;
+
+const Region = styled.p`
+  color: #222;
+  font-size: 1.2rem;
+`;
+
+const Population = styled.p`
+  color: #222;
+  font-size: 1.125rem;
 `;
 
 export default CountryCard;
